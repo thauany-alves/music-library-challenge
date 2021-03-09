1b755198590513aa28a2d7cb850e59565680ca64
thauany-alves <thauanyaraujoalves@hotmail.com>
2021-03-09 19:29:56 -0300

    Adding Search component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
 import Head from 'next/head';
+import Search from '../components/Search';
 import Sidebar from '../components/Sidebar';
+
+import styles from '../styles/Home.module.css';
 
 function HomePage() {
   return (
@@ -7,7 +10,13 @@ function HomePage() {
       <Head>
         <title>Inicio | Music</title>
       </Head>
-      <Sidebar></Sidebar>
+      <div className={styles.container}>
+        <Sidebar></Sidebar>
+        <div className={styles.dashboard}>
+          <Search />
+        </div>
+      </div>
+      
     </div>
   );
   
